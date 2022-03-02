94bdf2931ab5e4c5795e62874595465d4129c296
Danil Chernov <danilchernov-developer@yandex.ru>
2022-03-02 22:17:31 +0700

    feat: added const with http codes

--- src/constants.js
+++ src/constants.js
@@ -4,3 +4,11 @@ module.exports.EXIT_CODE = {
   SUCCESS: 0,
   UNCAUGHT_FATAL_EXCEPTION: 1
 };
+
+module.exports.HTTP_CODE = {
+  OK: 200,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+  FORBIDDEN: 403,
+  UNAUTHORIZED: 401,
+};
