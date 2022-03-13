9eb52de87936b23f17fc07119915fb4285d08d52
Danil Chernov <danilchernov-developer@yandex.ru>
2022-03-13 11:15:58 +0700

    feat: connected pug in express

--- src/express/express.js
+++ src/express/express.js
@@ -12,6 +12,9 @@ const PUBLIC_DIR = `public`;
 
 const app = express();
 
+app.set(`views`, path.resolve(__dirname, `templates`));
+app.set(`view engine`, `pug`);
+
 app.use(express.static(path.resolve(__dirname, PUBLIC_DIR)));
 app.use(`/`, mainRoutes);
 app.use(`/articles`, articlesRoutes);
