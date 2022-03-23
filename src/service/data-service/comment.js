8efc27f0b61e7bd2d5b5e2f47f7a0f8fc6f924b5
Danil Chernov <danilchernov-developer@yandex.ru>
2022-03-23 18:06:10 +0700

    feat(service): added "comment" service

---
+++ src/service/data-service/comment.js
@@ -0,0 +1,36 @@
+'use strict';
+
+const {nanoid} = require(`nanoid`);
+const {MAX_ID_LENGTH} = require(`../../constants`);
+
+class CommentService {
+  create(comment, article) {
+    const newComment = Object.assign({id: nanoid(MAX_ID_LENGTH)}, comment);
+
+    article.comments.push(newComment);
+
+    return newComment;
+  }
+
+  delete(id, article) {
+    const comment = this.findOne(id, article);
+
+    if (!comment) {
+      return null;
+    }
+
+    article.comments = article.comments.filter((item) => item.id !== id);
+
+    return comment;
+  }
+
+  findOne(id, article) {
+    return article.comments.find((comment) => comment.id === id);
+  }
+
+  findAll(article) {
+    return article.comments;
+  }
+}
+
+module.exports = CommentService;
