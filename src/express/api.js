2235846a4aa905de1c1162bb4fe1445f7526b1c2
Danil Chernov <danilchernov-developer@yandex.ru>
2022-03-30 18:44:03 +0700

    feat: added api-client

---
+++ src/express/api.js
@@ -0,0 +1,49 @@
+"use strict";
+
+const axios = require(`axios`);
+
+const TIMEOUT = 1000;
+
+const port = process.env.API_PORT || 3000;
+const defaultUrl = `http://localhost:${port}/api/`;
+
+class API {
+  constructor(baseURL, timeout) {
+    this._http = axios.create({
+      baseURL,
+      timeout,
+    });
+  }
+
+  async _load(url, options) {
+    const response = await this._http.request({ url, ...options });
+    return response.data;
+  }
+
+  async getArticles() {
+    return await this._load(`/articles`);
+  }
+
+  async getArticle(id) {
+    return await this._load(`/articles/${id}`);
+  }
+
+  async search(query) {
+    return await this._load(`/search`, { params: query });
+  }
+
+  async getCategories() {
+    return await this.load(`/category`);
+  }
+
+  async createArticle(data) {
+    return await this.load(`/articles`, {
+      method: `POST`,
+      data,
+    });
+  }
+}
+
+const defaultAPI = new API(defaultUrl, TIMEOUT);
+
+module.exports = { API, getApi: () => defaultAPI };
