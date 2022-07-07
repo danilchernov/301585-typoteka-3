895a2270ab283776915fb58d4f2cf04d53d5c5d8
Danil Chernov <danilchernov-developer@yandex.ru>
2022-07-07 15:18:17 +0700

    refactor(mentor): :recycle: corrections from the mentor

--- src/lib/jwt.js
+++ src/lib/jwt.js
@@ -2,8 +2,10 @@
 
 const jwt = require(`jsonwebtoken`);
 
+const EXPIRES_IN = `1d`;
+
 module.exports.generateAccessToken = (tokenData) => {
-  return jwt.sign(tokenData, process.env.JWT_SECRET, { expiresIn: `1d` });
+  return jwt.sign(tokenData, process.env.JWT_SECRET, { expiresIn: EXPIRES_IN });
 };
 
 module.exports.verifyAccessToken = (token) => {
